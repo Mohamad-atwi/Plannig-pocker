796848e218043d8488b54ae14ec0b40396a9fc62
MostaphaY8 <Mostaphayessf14@gmail.com>
2023-03-26 23:45:59 +0200

    Card+Deck

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import BasicCard from "../src/components/card";
+import Overflow from "./components/deck-scroller/DeckCard";
 function App() {
   return (
     <div className="App">
       <h1>Welcome To The TSD-Project</h1>
       <p>Choose the Card</p>
-      <BasicCard />
+      <Overflow />
     </div>
   );
 }
--- frontend/src/components/card.js
+++ frontend/src/components/card.js
@@ -11,7 +11,7 @@ import { Grid } from "@mui/material";
 //     justifyContent: "center",
 //   },
 // });
-const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
 export default function BasicCard() {
   // const classes = useStyles();
 
