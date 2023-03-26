150823343435f82c52715d6e3265cd5b55948680
MostaphaY8 <Mostaphayessf14@gmail.com>
2023-03-26 22:48:51 +0200

    Card creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
+import BasicCard from "../src/components/card";
 function App() {
   return (
     <div className="App">
       <h1>Welcome To The TSD-Project</h1>
+      <p>Choose the Card</p>
+      <BasicCard />
     </div>
   );
 }
--- frontend/src/components/card.js
+++ frontend/src/components/card.js
@@ -1,7 +1,57 @@
-import React from "react";
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { Grid } from "@mui/material";
+// import { makeStyles } from "@mui/material";
+// const useStyles = makeStyles({
+//   card: {
+//     width: "5rem",
+//     display: "flex",
+//     justifyContent: "center",
+//   },
+// });
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+export default function BasicCard() {
+  // const classes = useStyles();
 
-const card = () => {
-  return <div>card</div>;
-};
-
-export default card;
+  return (
+    <Grid
+      container
+      spacing={1}
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+    >
+      {numbers.map((number) => (
+        <Grid item xs={1} md={1}>
+          <Card
+            sx={{
+              width: "6rem",
+              height: "8rem",
+              display: "flex",
+              justifyContent: "center",
+              outline: "0.2rem",
+              outlineStyle: "solid",
+            }}
+            // className={classes.card}
+          >
+            <CardContent>
+              <Typography
+                variant="h2"
+                component="div"
+                style={{
+                  justifyContent: "center",
+                  textAlign: "center",
+                  marginTop: "1rem",
+                }}
+              >
+                {number}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
