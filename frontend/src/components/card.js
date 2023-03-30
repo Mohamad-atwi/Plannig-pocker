189621937f67186bb53897318775ea7233e78f2a
MostaphaY8 <Mostaphayessf14@gmail.com>
2023-03-31 00:02:47 +0200

    Select card

--- frontend/src/components/card.js
+++ frontend/src/components/card.js
@@ -1,12 +1,16 @@
-import * as React from "react";
+import { useState } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 
-export default function BasicCard({ number }) {
-
+export default function BasicCard({ number, setSelectedCard, selectedCard }) {
+  const handleClick = () => {
+    setSelectedCard(number);
+    console.log("card selected ", number);
+  };
   return (
     <Card
+      onClick={handleClick}
       sx={{
         width: "6rem",
         height: "8rem",
@@ -14,8 +18,10 @@ export default function BasicCard({ number }) {
         justifyContent: "center",
         outline: "0.2rem",
         outlineStyle: "solid",
+        color: selectedCard === number ? "gold" : "black",
+        backgroundColor: selectedCard === number ? "green" : "white",
       }}
-    // className={classes.card}
+      // className={classes.card}
     >
       <CardContent>
         <Typography
