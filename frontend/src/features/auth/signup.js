01fdac08e2a4f87bdbaf83b0d722b05f4c0b1a9d
MostaphaY8 <Mostaphayessf14@gmail.com>
2023-03-31 15:44:10 +0200

    signup Form with route

--- frontend/src/features/auth/signup.js
+++ frontend/src/features/auth/signup.js
@@ -1,41 +1,70 @@
 import { useState } from "react";
-import { TextField, Button, Grid } from "@mui/material/";
+import { TextField, Button, Grid, Paper } from "@mui/material/";
+import { useNavigate } from "react-router-dom";
 
 function SignupForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    navigate("/home", true);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Grid container spacing={2} justify="center">
-        <Grid item xs={12} sm={6}>
-          <TextField
-            label="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            label="Password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Button type="submit" variant="contained" color="primary" fullWidth>
-            Signup
-          </Button>
-        </Grid>
-      </Grid>
-    </form>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Paper
+        elevation={7}
+        style={{
+          padding: 20,
+          width: "15rem",
+
+          margin: 0,
+          justifyContent: "center",
+          textAlign: "center",
+        }}
+      >
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={2} direction="column">
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                // fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="Password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                // fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                //  fullWidth
+              >
+                Signup
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+    </div>
   );
 }
 
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,15 +2,16 @@ import React, { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LinearProgress } from "@mui/material";
 
-import SignupForm from "../features/auth/SignUp";
-
+import SignupForm from "../features/auth/signup";
+import Home from "../features/home/home";
 const index = () => {
   return (
     <Router forceRefresh={true}>
       <Suspense fallback={<LinearProgress />}>
         <Routes>
           {/* Public URL */}
-          <Route index path="/signup" element={<SignupForm />} />
+          <Route index path="/" element={<SignupForm />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </Suspense>
     </Router>
