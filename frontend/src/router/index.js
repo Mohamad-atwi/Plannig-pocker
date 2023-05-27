cd60f06260127ccc566053734c6cb55b01171d47
mostapha <mostaphayessf14@gmail.com>
2023-05-27 14:08:35 +0200

    GDBR

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,10 @@
 import React, { Suspense } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { LinearProgress } from "@mui/material";
 
 import SignupForm from "../features/auth/signup";
@@ -10,27 +15,26 @@ import PrivateRoute from "./PrivateRoutes";
 import JoinsSession from "../features/session/JoinSession";
 import CreateSession from "../features/session/CreateSession";
 import LayoutSideBar from "../layout/layout-sidebar";
+import TermsAndConditions from "../pages/TermsAndConditions";
 const index = () => {
-
   return (
     <Router forceRefresh={true}>
       <Suspense fallback={<LinearProgress />}>
         <Routes>
-          <Route exact path='/' element={<PrivateRoute />}>
-
+          <Route exact path="/" element={<PrivateRoute />}>
             <Route path="/" element={<LayoutSideBar />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/session/:sessionId" element={<SessioPage />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/session/:sessionId" element={<SessioPage />} />
             </Route>
 
             <Route path="/JoinSession" element={<JoinsSession />} />
             <Route path="/CreateSession" element={<CreateSession />} />
-
           </Route>
-          <Route path='/signup' element={<SignupForm />} />
-          <Route path='/login' element={<LoginForm />} />
+          <Route path="/signup" element={<SignupForm />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/Terms" element={<TermsAndConditions />} />
 
-          <Route path='*' element={<Navigate to='/' />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Suspense>
     </Router>
