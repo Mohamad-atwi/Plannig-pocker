9c50c7326b586fa8febca2a5b13f23374b8bfd79
hassankh0 <hassan.h.khalife@outlook.com>
2023-06-04 13:12:54 +0200

    data gen

--- backend/database/migrations/2023_04_30_222616_create_user_estimations_table.php
+++ backend/database/migrations/2023_04_30_222616_create_user_estimations_table.php
@@ -17,10 +17,10 @@ return new class extends Migration
             $table->unsignedBigInteger('session_id');
             $table->unsignedBigInteger('card_id');
             $table->unsignedBigInteger('story_id');
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('session_id')->references('id')->on('sessions');
-            $table->foreign('card_id')->references('id')->on('cards');
-            $table->foreign('story_id')->references('id')->on('stories');
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->foreign('session_id')->references('id')->on('sessions')->cascadeOnDelete();
+            $table->foreign('card_id')->references('id')->on('cards')->cascadeOnDelete();
+            $table->foreign('story_id')->references('id')->on('stories')->cascadeOnDelete();
             $table->timestamps();
         });
     }
--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -13,11 +13,40 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         \App\Models\Deck::factory()->create();
-        \App\Models\Card::factory(10)->create();
-        \App\Models\User::factory(10)->create();
-        \App\Models\Session::factory(1)->create();
-        \App\Models\Storie::factory(10)->create();
-        \App\Models\UserEstimation::factory(3)->create();
+        \App\Models\Card::factory()->create([
+            'value' => 1
+        ]);
+        \App\Models\Card::factory()->create([
+            'value' => 2
+        ]);
+        \App\Models\Card::factory()->create([
+            'value' => 3
+        ]);
+        \App\Models\Card::factory()->create([
+            'value' => 4
+        ]);
+        \App\Models\Card::factory()->create([
+            'value' => 5
+        ]);
+        \App\Models\Card::factory()->create([
+            'value' => 6
+        ]);
+        \App\Models\Card::factory()->create([
+            'value' => 7
+        ]);
+        \App\Models\Card::factory()->create([
+            'value' => 8
+        ]);
+        \App\Models\Card::factory()->create([
+            'value' => 9
+        ]);
+        \App\Models\Card::factory()->create([
+            'value' => 10
+        ]);
+        // \App\Models\User::factory(10)->create();
+        // \App\Models\Session::factory(1)->create();
+        // \App\Models\Storie::factory(10)->create();
+        // \App\Models\UserEstimation::factory(3)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
